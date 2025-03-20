b59881d1d5a9f807b2eff378c83b178608580fb5
Sol Donoso <soldonoso2@gmail.com>
2025-03-20 20:30:31 -0300

    Continuacion movimiento

--- Escenas/Componentes/Jugador/jugador.gd
+++ Escenas/Componentes/Jugador/jugador.gd
@@ -1,11 +1,17 @@
 extends CharacterBody3D
 
+var velocidad : float = 10
+
 func _ready():
 	pass
 	
 func _process(delta):
-	if Input.is_action_pressed("mover_adelante"):
-		velocity.z = -10
-	else:
-		velocity.z = 0
-	move_and_slide()
+	
+		velocity.z = velocidad * Input.get_axis("mover_adelante","mover_atras")
+		velocity.x= velocidad * Input.get_axis("mover_derecha","mover_izquierda")
+		Input.get_axis("mover_adelante","mover_atras")
+		Input.get_axis("mover_derecha","mover_izquierda")
+		
+		
+	
+		move_and_slide()
