75fec05053eb147cdce04b7ddc10ade6179ec848
ACID THEORY <112581133+acidtheory@users.noreply.github.com>
2025-03-20 21:34:36 -0300

    Movimiento basico de jugador finalizado

--- Escenas/Componentes/Jugador/jugador.gd
+++ Escenas/Componentes/Jugador/jugador.gd
@@ -1,17 +1,23 @@
 extends CharacterBody3D
 
-var velocidad : float = 10
+@export var velocidad : float = 10
+@export var mouse_sens : float = 0.025
+@onready var camera : Camera3D = get_node("Camera3D")
 
 func _ready():
-	pass
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
-func _process(delta):
-	
+func _physics_process(delta):
+		velocity.y = -10
 		velocity.z = velocidad * Input.get_axis("mover_adelante","mover_atras")
-		velocity.x= velocidad * Input.get_axis("mover_derecha","mover_izquierda")
-		Input.get_axis("mover_adelante","mover_atras")
-		Input.get_axis("mover_derecha","mover_izquierda")
-		
-		
-	
+		velocity.x= velocidad * Input.get_axis("mover_izquierda","mover_derecha")
+		velocity = velocity.rotated(Vector3(0,1,0),camera.rotation.y)
+		if Input.is_action_just_pressed("saltar"):
+			velocity.y = 100
 		move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		camera.rotation.x = camera.rotation.x - event.relative.y * 0.05
+		camera.rotation.y = camera.rotation.y - event.relative.x * 0.05
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x,-90,90)
