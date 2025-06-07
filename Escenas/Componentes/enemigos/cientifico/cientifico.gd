7e136553be6bbe3ff8373bc7a80a7dfecd353ec6
ACID THEORY <112581133+acidtheory@users.noreply.github.com>
2025-06-07 18:36:21 -0300

    Correccion inicial en cientifico.gd

--- Escenas/Componentes/enemigos/cientifico/cientifico.gd
+++ Escenas/Componentes/enemigos/cientifico/cientifico.gd
@@ -27,6 +27,7 @@ func _physics_process(delta: float) -> void:
 	# hace que las funciones de procesamiento cambien en relacion al estado
 	match Globals.nivel.estado_alerta:
 		Globals.nivel.Alerta: procesar_alerta()
+		Globals.nivel.Caza: procesar_alerta() #Un detalle mas, cuando entra al estado de caza tambien queremos que vaya al area segura, asi que lo puse aca tambien en el match. 
 		Globals.nivel.Patrulla: procesar_patrulla()
 	procesar_estados()
 
@@ -51,24 +52,35 @@ func procesar_estados():
 
 ## Codigo que corre cada process frame mientras el estado sea de patrulla
 func procesar_patrulla():
-	var patrulla : Node = get_node_or_null("Patrulla")
-	if patrulla:
-		if lista_puntos.size() == 0:
-			lista_puntos = patrulla.get_children()
-			lista_puntos.shuffle()
-			agent.target_position = lista_puntos.pop_front().position
-		else:
-			# direccion al siguiente paso hacia la target_position
-			var distancia_a_punto : Vector3 = global_position.direction_to(agent.get_next_path_position())
-			
-			#se mueve hacia esa direccion multiplicado por su velocidad
-			velocity = distancia_a_punto.normalized() * velocidad
-			velocity.y = 0
-			
-			#si la distancia a la target_position es menor a 1, ir al siguiente punto.
-			if global_position.distance_to(agent.target_position) < 1:
-				indice_punto += 1
-				agent.target_position = lista_puntos[indice_punto].global_position
+	# Aca esta el codigo anterior de procesar_patrulla.
+	# Aca copiaste la parte que utilizaba una manera distinta de sacar la lista de puntos usando los hijos de un nodo llamado Patrulla
+	# Si te fijas, te vas a dar cuenta que todo el codigo que pusimos la clase pasada que se encarga del comportamiento del cientifico esta dentro de un if que siempre va a dar true.
+	# Por ende, el navigation agent nunca asigna su target position, y nunca se asigna la direccion del cientifico tampoco
+	# Ademas, un pequeño detalle que a mi tambien se me paso de largo, despues de asignar la velocity tampoco habia un move_and_slide(), que siempre tiene que aplicarse despues de declarar la velocity para que se mueva.
+	#Ahora fijate si lo podes arreglar tambien en procesar_alerta()! 😄
+	
+	# Estas lineas de codigo aca abajo comentadas son sobras del codigo anterior que son innecesarias en la nueva implementacion y estaban deteniendo el resto del codigo de correr:
+	#var patrulla : Node = get_node_or_null("Patrulla")
+	#if patrulla:
+		#if lista_puntos.size() == 0:
+			#lista_puntos = patrulla.get_children()
+			#lista_puntos.shuffle()
+			#agent.target_position = lista_puntos.pop_front().position
+		#else:
+
+	# direccion al siguiente paso hacia la target_position
+	var distancia_a_punto : Vector3 = global_position.direction_to(agent.get_next_path_position())
+	
+	#se mueve hacia esa direccion multiplicado por su velocidad
+	velocity = distancia_a_punto.normalized() * velocidad
+	velocity.y = 0
+	print(distancia_a_punto)
+	move_and_slide()
+	#si la distancia a la target_position es menor a 1, ir al siguiente punto.
+	if global_position.distance_to(agent.target_position) < 1:
+		indice_punto += 1
+		agent.target_position = lista_puntos[indice_punto].global_position
+	
 
 ## Codigo que corre cada process frame mientras el estado sea de alerta
 func procesar_alerta():
