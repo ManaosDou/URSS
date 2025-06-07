ae824e15850ead4e1178738a4c394408697ecf4d
ManaosDou <lucastimoteoarano@gmail.com>
2025-06-07 17:39:51 -0300

    clase nivel

--- Escenas/Componentes/niveles/clase_nivel.gd
+++ Escenas/Componentes/niveles/clase_nivel.gd
@@ -3,4 +3,7 @@ class_name Nivel
 
 var triggers : Dictionary = {"jugador_salto" : false}
 enum {Patrulla,Alerta,Caza}
-var estado_alerta = Patrulla
+@export var estado_alerta = Patrulla
+
+func _ready() -> void:
+	Globals.nivel = self
